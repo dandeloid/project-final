7d02f538baa746ed04cc34affedfad155bfd2564
dandeloid <82587220+dandeloid@users.noreply.github.com>
2022-01-21 15:48:50 +0100

    fetch data

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
 import Product from './Product'
-import data from '../data' 
+import data from '../data.js' 
 
 const HomeScreen = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+
+        fetch(('https://a-book-api-2.herokuapp.com/books'), options)
+            .then((res) => res.json())
+            .then((data) => {
+                setProducts(data)
+            })
+    }, [])
+    console.log("test", products) ///<<<<< NEEED TO GET FETCH TO SHOW
+
     return (
         <div className="row center">
             {data.products.map(product => (
